989c6753d83fdfe43a0e4a1c0fef09ce93cf5f1b
aleivc <aleivc@163.com>
2021-05-04 11:49:02 +0800

    init project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>aleivc.com</h1>
+      <h2>that's my website, huha!</h2>
+    </div>
+  )
+}
