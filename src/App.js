6748d20fdcc246f953f5e108e28212e2d0bd40ba
aleivc <aleivc@163.com>
2021-10-28 12:31:33 +0800

    install node-sass@4 && init basic fram

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import Welcome from "./components/welcome/Welcome";
+import TopBar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Profile from "./components/profile/Profile";
+import Works from "./components/works/Works";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 function App() {
-  return <Welcome />;
+  return (
+		<div className="app">
+			<TopBar />
+			<div className="sections">
+				<Intro />
+				<Profile />
+				<Works />
+				<Testimonials />
+				<Contact />
+			</div>
+		</div>
+	)
 }
 
 export default App;
