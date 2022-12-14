bd21698a11bb8160751461571a6c044df7e36659
aleivc <aleivc@outlook.com>
2022-12-14 13:38:29 +0800

    mqtt doc image

---
+++ src/pages/Others/Doc/index.js
@@ -0,0 +1,5 @@
+const Doc = () => {
+    return (<div><img src="/assets/images/doc.png" alt="doc"/></div>)
+}
+
+export default Doc;
--- src/pages/Others/index.js
+++ src/pages/Others/index.js
@@ -1,9 +1,30 @@
 import Tools from "./Tools";
+import Doc from './Doc';
+import {Tabs} from "antd";
 
 const Others = () => {
+    const onChange = (key) => {
+        console.log(key);
+    };
+
     return (
         <div className="p-3 h-screen overflow-scroll">
-            <Tools/>
+            <Tabs
+                defaultActiveKey="1"
+                onChange={onChange}
+                items={[
+                    {
+                        label: `启闭机`,
+                        key: '1',
+                        children: <Tools />,
+                    },
+                    {
+                        label: `协议预览`,
+                        key: '2',
+                        children: <Doc />,
+                    },
+                ]}
+            />
         </div>
     );
 };
